0f618f716724561399cb579cb79356817b998915
AmulyaCresen <amulya.chodisetty@cresensolutions.com>
2026-03-31 12:52:30 +0530

    updated application config

--- src/main/java/com/config_service/ConfigServiceApplication.java
+++ src/main/java/com/config_service/ConfigServiceApplication.java
@@ -1,13 +1,12 @@
 package com.config_service;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
-
 }
